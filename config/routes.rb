17d52593cc3446c1facb81a1784de8b8fb22c8ee
Gabriel Martinez <ghmartinez96@hotmail.com>
2022-11-22 13:07:24 +0100

    Fixed duplicated services routing.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :services, only: [:index, :new, :create, :destroy, :show]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
   resources :services
 end
